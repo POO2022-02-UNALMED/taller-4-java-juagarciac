897390a4072ea9859a3e3e06c9a25c0abba7323b
juagarciac <111471638+juagarciac@users.noreply.github.com>
2022-09-03 19:42:55 -0500

    b

--- src/main/java/classroom/Tipo.java
+++ src/main/java/classroom/Tipo.java
@@ -6,13 +6,13 @@ public class Tipo {
 	public Tipo(int codigo) {
 		this.codigo = codigo;
 	}
-	public Tipo DISCIPLINAR() {
-	return new Tipo(int 10);
+	public static Tipo DISCIPLINAR() {
+		return new Tipo(int 10);
 	}
-	public Tipo FUNDAMENTACION() {
+	public static Tipo FUNDAMENTACION() {
 		return new Tipo(int 20);
 	}
-	public Tipo ELECTIVA() {
+	public static Tipo ELECTIVA() {
 		return new Tipo(int 30);
 	}
 }
