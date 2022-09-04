a6ba1ea42f497e81893f724c963e33a801d5d4c4
juagarciac <111471638+juagarciac@users.noreply.github.com>
2022-09-03 21:40:21 -0500

    j

--- src/test/main/java/classroom/Tipo.java
+++ src/test/main/java/classroom/Tipo.java
@@ -1,18 +1,9 @@
 package classroom;
 
-public class Tipo {
+public enum Tipo {
+	DISCIPLINAR(10),FUNDAMENTACION(20),ELECTIVA(30)
 	public int codigo;
 	
-	public Tipo(int codigo) {
+	private Tipo(int codigo) {
 		this.codigo = codigo;
 	}
-	public static Tipo DISCIPLINAR() {
-		return new Tipo(int 10);
-	}
-	public static Tipo FUNDAMENTACION() {
-		return new Tipo(int 20);
-	}
-	public static Tipo ELECTIVA() {
-		return new Tipo(int 30);
-	}
-}
